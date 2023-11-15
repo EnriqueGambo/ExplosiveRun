1f7cbe073dd65f16ae2566324bc24fbb76b2f0cc
EnriqueGambo <eag3231@mavs.uta.edu>
2023-11-15 13:28:52 -0600

    Camera for game

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Options : MonoBehaviour
 {
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
     }
 }
