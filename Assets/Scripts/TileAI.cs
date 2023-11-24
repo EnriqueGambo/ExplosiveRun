370414681cb53235e58eca8abd4ba933f9d34ff5
Jacob-Truelove <jet4853@mavs.uta.edu>
2023-11-23 21:00:31 -0600

    This is just to be safe

--- Assets/Scripts/TileAI.cs
+++ Assets/Scripts/TileAI.cs
@@ -7,17 +7,27 @@ public class TileAI : MonoBehaviour
 {
     [SerializeField] private Collider2D event_collider;
     [SerializeField] private Collider2D player;
+    [SerializeField] private GameObject explosion;
+    [SerializeField] private Collider2D[] checks;
     private bool has_touched = false;
     private Stopwatch sw = new Stopwatch();
+    private Stopwatch anim = new Stopwatch();
 
     // Start is called before the first frame update
 
     // Update is called once per frame
+    private int dir = 0;
 
+    private bool has_got = false;
     void Update()
     {
         if (Contact())
         {
+            if (!has_got)
+            {
+                dir = direction();
+                has_got = true;
+            }
             sw.Start();
             if (sw.Elapsed.Milliseconds > 500)
                 explode();
@@ -30,10 +40,33 @@ public class TileAI : MonoBehaviour
 
     private void explode()
     {
+        anim.Start();
+
+        Explosion exp = explosion.GetComponent<Explosion>();
+        exp.Player = player;
+        exp.choice = dir;
+        exp.stays = false;
+
+        Quaternion rot = new Quaternion(0, 0, 180, 0);
+        Instantiate(explosion, transform.position, rot);
+        
+
         Destroy(gameObject);
     }
     private bool Contact()
     {
         return event_collider.IsTouching(player);
+    }
+    private int direction()
+    {
+        if (checks[0].IsTouching(player))
+            return 0;
+        else if (checks[1].IsTouching(player))
+            return 1;
+        else if (checks[2].IsTouching(player))
+            return 2;
+        if (checks[3].IsTouching(player))
+            return 3;
+        return 0;
     }
 }
